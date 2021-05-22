3822528a485a2d232dc51f19f9bc62c1e5102ece
Alexey <oglok123@rambler.ru>
2021-05-23 00:07:49 +0300

    04_Encapsulation

---
+++ 04_Encapsulation/01_Elevator/src/Elevator.java
@@ -0,0 +1,32 @@
+public class Elevator {
+    private int currentFloor = 1;
+    private int minFloor;
+    private int maxFloor;
+
+    public Elevator (int minFloor, int maxFloor){
+        this.minFloor = this.minFloor + minFloor;
+        this.maxFloor = this.maxFloor + maxFloor;
+    }
+    private int getCurrentFloor() {
+        return currentFloor;
+    }
+    private void moveDown (){
+        currentFloor = currentFloor - 1;
+    }
+    private void moveUp(){
+        currentFloor = currentFloor + 1;
+    }
+    public void move(int floor){
+        if (floor < minFloor || floor > maxFloor || floor == currentFloor ){
+            System.out.println("Неверный номер этажа");
+            return;
+        }
+        for (int i = floor; i > currentFloor; moveUp()){
+            System.out.println("Этаж " + currentFloor);
+        }
+        for (int i = floor; i < currentFloor; moveDown()){
+            System.out.println("Этаж " + currentFloor);
+        }
+        System.out.println("Этаж " + currentFloor);
+    }
+}
