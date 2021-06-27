2a6a4a6d25ad4ef132a3db757eb2ab2e332df807
Alexey <oglok123@rambler.ru>
2021-06-27 21:09:20 +0300

    update fullname 3

--- 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
@@ -18,47 +18,53 @@ public class Main {
       String patronymic = !input.contains(" ") ? input : input.substring(input.lastIndexOf(" ") + 1);
       String tire = "-";
 
+      if (surname.charAt(0) == tire.charAt(0) || surname.charAt(surname.length() - 1) == tire.charAt(0)){
+        System.out.println("Введенная строка не является ФИО");
+        continue;
+      }
+
+      if (name.charAt(0) == tire.charAt(0) || name.charAt(name.length() - 1) == tire.charAt(0)){
+        System.out.println("Введенная строка не является ФИО");
+        continue;
+      }
+
+      if (patronymic.charAt(0) == tire.charAt(0) || patronymic.charAt(patronymic.length() - 1) == tire.charAt(0)){
+        System.out.println("Введенная строка не является ФИО");
+        continue;
+      }
+
       surname = !surname.contains("-") ? (surname.substring(0,1).toUpperCase() + surname.substring(1).toLowerCase()) : surname.substring(0,1).toUpperCase() + surname.substring(1, surname.indexOf("-")).toLowerCase() + "-" + surname.substring(surname.indexOf("-") + 1,surname.indexOf("-") + 2).toUpperCase() + surname.substring(surname.indexOf("-") + 2).toLowerCase();
-      name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
-      patronymic = patronymic.substring(0,1).toUpperCase() + patronymic.substring(1).toLowerCase();
+      name = !name.contains("-") ? (name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase()) : name.substring(0,1).toUpperCase() + name.substring(1, name.indexOf("-")).toLowerCase() + "-" + name.substring(name.indexOf("-") + 1,name.indexOf("-") + 2).toUpperCase() + name.substring(name.indexOf("-") + 2).toLowerCase();
+      patronymic = !patronymic.contains("-") ? (patronymic.substring(0,1).toUpperCase() + patronymic.substring(1).toLowerCase()) : patronymic.substring(0,1).toUpperCase() + patronymic.substring(1, patronymic.indexOf("-")).toLowerCase() + "-" + patronymic.substring(patronymic.indexOf("-") + 1,patronymic.indexOf("-") + 2).toUpperCase() + patronymic.substring(patronymic.indexOf("-") + 2).toLowerCase();
 
       if  ( input.indexOf(" ") == input.lastIndexOf(" ")){
         System.out.println("Введенная строка не является ФИО");
-        return;
+        continue;
       }
 
       if( input.equals(surname) || input.equals(name) || input.equals(patronymic)){
         System.out.println("Введенная строка не является ФИО");
-        return;
+        continue;
+      }
+
+      if (name.contains(" ")){
+        System.out.println("Введенная строка не является ФИО");
+        continue;
       }
 
       for (int i = 0; i < input.length(); i++){
-        for (int y = 0; y < name.length(); y++){         // проверка имени на символы
-          char b = name.charAt(y);
-          if (!Character.isAlphabetic(b)){
-            System.out.println("Введенная строка не является ФИО");
-            return;
-          }
-        }
-        for (int z = 0; z < patronymic.length(); z++){  // проверка отчества на символы
-          char e = patronymic.charAt(z);
-          if (!Character.isAlphabetic(e)){
-            System.out.println("Введенная строка не является ФИО");
-            return;
-          }
-        }
         char a = input.charAt(i);
-        if( !Character.isAlphabetic(a)){
-          if (Character.isSpaceChar(a)){
+        if( !Character.isAlphabetic(a)) {
+
+          if (Character.isSpaceChar(a)) {
             continue;
           }
-          if (surname.indexOf("-") != surname.lastIndexOf("-")){
-
+          if (surname.indexOf("-") != surname.lastIndexOf("-") || name.indexOf("-") != name.lastIndexOf("-") || patronymic.indexOf("-") != patronymic.lastIndexOf("-")) {
             System.out.println("Введенная строка не является ФИО");
-            return;
+            break;
           }
 
-          if (a == tire.charAt(0)){
+          if (a == tire.charAt(0)) {
             continue;
           }
 
@@ -66,11 +72,9 @@ public class Main {
           return;
         }
       }
-      if (name.contains(" ")){
-        System.out.println("Введенная строка не является ФИО");
-        return;
-      }
-      System.out.println("Фамилия: " + surname + System.lineSeparator() + "Имя: " + name + System.lineSeparator() + "Отчество: " + patronymic);
+
+
+        System.out.println("Фамилия: " + surname + System.lineSeparator() + "Имя: " + name + System.lineSeparator() + "Отчество: " + patronymic);
 
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
     }
