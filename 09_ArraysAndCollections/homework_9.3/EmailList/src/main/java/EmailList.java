2113b6f07feb930cf7260a92db40dcb5776eb285
Alexey <oglok123@rambler.ru>
2021-07-11 20:13:13 +0300

    homework_9.3/

---
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -0,0 +1,28 @@
+import org.apache.commons.collections.list.TreeList;
+
+import java.util.*;
+
+public class EmailList {
+    ArrayList<String> mail = new ArrayList<>();
+
+    public void add(String email) {
+        // TODO: валидный формат email добавляется
+
+        if (email.contains("@") && email.contains(".") && email.indexOf("@") == email.lastIndexOf("@") && email.indexOf(".") == email.lastIndexOf(".")) {
+
+            mail.add(email.toLowerCase(Locale.ROOT));
+
+        }
+    }
+
+
+    public List<String> getSortedEmails() {
+        // TODO: возвращается список электронных адресов в алфавитном порядке
+            TreeSet<String> mails = new TreeSet<>();
+            mails.addAll(mail);
+            mail.clear();
+            mail.addAll(mails);
+        return mail;
+    }
+
+}
---
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
+    private static EmailList emailList = new EmailList();
+
+
+    /* TODO:
+        Пример вывода списка Email, после ввода команды LIST в консоль:
+        test@test.com
+        hello@mail.ru
+        - каждый адрес с новой строки
+        - список должен быть отсортирован по алфавиту
+        - email в разных регистрах считается одинаковыми
+           hello@skillbox.ru == HeLLO@SKILLbox.RU
+        - вывод на печать должен быть в нижнем регистре
+           hello@skillbox.ru
+        Пример вывода сообщения об ошибке при неверном формате Email:
+        "Неверный формат email"
+    */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+
+
+            }
+
+            //TODO: write code here
+
+            String[] arr = input.split(",?\\s+");
+
+            if (arr.length < 2 && !arr[0].equals("LIST")){
+                continue;
+            }
+
+            if (input.contains("ADD")){
+
+                emailList.add(input.substring(input.indexOf(" ")));
+            }
+
+
+            if(input.contains("LIST")){
+
+                System.out.println(emailList.getSortedEmails());
+                continue;
+            }
+            if (!input.contains("@") || !input.contains(".") || input.indexOf("@") != input.lastIndexOf("@") || input.indexOf(".") != input.lastIndexOf(".")) {
+
+
+                System.out.println(WRONG_EMAIL_ANSWER);
+            }
+
+
+        }
+    }
+}
