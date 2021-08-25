9f3cfd7a430531a736946a41750205d70b65ea19
Alexey <oglok123@rambler.ru>
2021-08-25 16:30:17 +0300

    add homework_11.2

---
+++ 11_AdvancedOOPFeatures/homework_11.2/Employees/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    private static final String STAFF_TXT = "data/staff.txt";
+
+    public static void main(String[] args) {
+        List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
+        Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, 2017);
+        System.out.println(employeeMaxSalary);
+    }
+
+    public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
+        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
+        // кто пришёл в году, указанном в переменной year
+
+     return    staff.stream().sorted(Comparator.comparing(Employee::getWorkStart)).filter(x -> x.getWorkStart().getYear() >= year).max(Comparator.comparing(Employee::getSalary)).get();
+
+    }
+}
